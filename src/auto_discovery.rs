900dd12e8f744bb59ea41191d9368125431db94c
jumpiix <david@unterguggenberger.ch>
2025-10-30 10:30:20 +0100

    google captcha handling

--- src/auto_discovery.rs
+++ src/auto_discovery.rs
@@ -86,7 +86,7 @@ impl AutoDiscovery {
         };
 
         let browser = Browser::new(LaunchOptions {
-            headless: false,
+            headless: true,
             sandbox: false,
             user_data_dir: Some(chrome_data),
             window_size: Some((1920, 1080)),
--- src/knx_client.rs
+++ src/knx_client.rs
@@ -277,7 +277,7 @@ impl KnxClient {
         info!("Launching headless Chrome...");
 
         let browser = Browser::new(LaunchOptions {
-            headless: false,
+            headless: true,
             sandbox: false,
             args: vec![
                 std::ffi::OsStr::new("--disable-blink-features=AutomationControlled"),
